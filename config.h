516ed52498f002fd82364056fc10559401fe7aac
x230 Admin <admin@x230.local>
2026-04-29 20:18:28 +0100

    x230 dwmblocks config: date/time + WiFi status
    - Block 1: date "+%a, %d %b 󰃭 %H:%M" (30s interval)
    - Block 2: /home/admin/Projects/dwmblocks/bin/wifi-menu.sh (5s)
    - Delimiter: \x1f (unit separator)
    - Added wifi-menu.sh for rofi WiFi network selection
    
    Refs: x230 bare metal setup

--- config.h
+++ config.h
@@ -1,11 +1,10 @@
 /* modify this file to change what commands output to your statusbar, and
  * recompile using the make command */
+
 static const Block blocks[] = {
 	/* command                interval    signal */
-        { "cat ~/.cache/dwmblocks/news",        0,        5 },
-	{ "$HOME/dwmblocks/weather.sh",         300,      0 },
 	{ "date '+%a, %d %b 󰃭 %H:%M'",            30,      0 },
-        { "$HOME/dwmblocks/wifi.sh",            5,        1 },
+	{ "/home/admin/Projects/dwmblocks/wifi.sh",            5,        1 },
 };
 
 /* sets delimeter between status commands. NULL character ('\0') means no
